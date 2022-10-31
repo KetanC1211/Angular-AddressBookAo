bfb824b3acd48ea473d9ddd607dd6c05f969b948
KetanC1211 <ketanchindarkar12@gmail.com>
2022-10-31 17:51:18 +0530

    Added Routing and Dashboard Component

--- AddressBookApp/src/app/app-routing.module.ts
+++ AddressBookApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbaordComponent } from './components/dashbaord/dashbaord.component';
+import { FormComponent } from './components/form/form.component'; 
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'dashboard',component:DashbaordComponent},
+  {path:"form",component:FormComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AddressBookApp/src/app/app.module.ts
+++ AddressBookApp/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { DashbaordComponent } from './components/dashbaord/dashbaord.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { FormComponent } from './components/form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { FormComponent } from './components/form/form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
