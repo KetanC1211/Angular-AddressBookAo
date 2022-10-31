4ffe4f4427271635286a1cfa9cc44dc7bcb812d8
KetanC1211 <ketanchindarkar12@gmail.com>
2022-10-31 15:35:53 +0530

    Created Project structure by adding component, service and model folder

--- AddressBookApp/src/app/app.module.ts
+++ AddressBookApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DashbaordComponent } from './components/dashbaord/dashbaord.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashbaordComponent,
+    PageNotFoundComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
